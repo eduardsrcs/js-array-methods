a74192bfad5f480c95dd6416e05d02d3e4e06d61
eduardsrcs <itbeard@inbox.lv>
2020-10-01 21:07:20 +0300

    map

--- js/loop2.js
+++ js/loop2.js
@@ -10,11 +10,19 @@ function renderTable(obj){
   //     <p>post ID: ${element.id}</p>`
   // });
 
-  for(let index in obj) {
-    out += `<h3>${obj[index].title}</h3>
-      <p>${obj[index].body}</p>
-      <p>Foreach</p>
-      <p>post ID: ${obj[index].id}</p>`
+  // for(let index in obj) {
+  //   out += `<h3>${obj[index].title}</h3>
+  //     <p>${obj[index].body}</p>
+  //     <p>For in</p>
+  //     <p>post ID: ${obj[index].id}</p>`
+  // }
+
+  for(let element of obj) {
+    out += `<h3>${element.title}</h3>
+      <p>${element.body}</p>
+      <p>For off</p>
+      
+      <p>post ID: ${element.id}</p>`
   }
 
   posts.innerHTML = out
---
+++ js/loop3.js
@@ -0,0 +1,4 @@
+const arr1 = [45, 4, 9, 16, 25]
+
+const arr2 = arr1.map(el => el * 2)
+console.log(arr2)
